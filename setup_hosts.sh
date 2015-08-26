bc72dfd8fc4ad3dfde8f9de51ad1ccc28a4862bf
Matthew Vincent <matt@averails.com>
2015-08-26 14:33:47 +0200

    updating repo layout

--- setup_hosts.sh
+++
@@ -1,7 +0,0 @@
-echo "#CAVE Nodes" >> /etc/hosts
-echo "192.168.10.1    headnode" >> /etc/hosts
-echo "192.168.10.2    CNEA-0" >> /etc/hosts
-echo "192.168.10.3    CNEA-1" >> /etc/hosts
-echo "192.168.10.4    CNEA-2" >> /etc/hosts
-echo "192.168.10.5    CNEA-3" >> /etc/hosts
-echo "192.168.10.6    CNEA-4" >> /etc/hosts
